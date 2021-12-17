4bbec03ae10896928be602a86e13ef8afa23b7ef
webster <wbian618@gmail.com>
2021-12-16 21:46:27 -0500

    commit tree 2d85866944f467588e41a2f9f33049207444b713

---
+++ app/controllers/students_controller.rb
@@ -0,0 +1,33 @@
+class StudentsController < ApplicationController
+    def index
+        students = Student.all
+        render json: students, status: :ok
+        end
+    
+        def show
+        student = Student.find(params[:id])
+        render json: students, status: :ok
+        end
+    
+        def destroy
+            student = Student.find(params[:id])
+        student.destroy
+        render json: student, status: :ok
+        end
+
+        def create
+            student = Student.create(student_params)
+            render json: student, status: :created
+        end
+
+        def update
+            student = Student.find(params[:id])
+            student.update(student_params)
+            render json: student, status: :ok
+        end
+
+        private
+        def student_params
+        params.permit(:name, :major, :age, :instructor_id)
+        end
+end
